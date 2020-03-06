0ed2c1e98778c49cfe533019cf89167145e15fea
Penjani Chavula <chavulapenjani@gmail.com>
2020-03-06 08:14:02 -0800

    use makefile instead of compile.sh

--- penji/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-# compile remove_lines
-#gcc remove_lines.c -o remove_lines -w -Ilibjpeg-turbo/build/ -Llibjpeg-turbo/build/ -ljpeg
-gcc remove_lines.c -o remove_lines -w -Ilibjpeg-turbo/ -Llibjpeg-turbo/build/ -Wl,-rpath=./libjpeg-turbo/build/ -ljpeg
