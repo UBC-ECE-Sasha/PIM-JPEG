0a10fc72bdc1cd86c33e73b4851aa8b19e2b5eda
Jackson Dagger <JacksonDDagger@gmail.com>
2022-01-08 02:18:10 +0100

    clean up image metrics script

--- metrics/README.md
+++ metrics/README.md
@@ -1,6 +1,10 @@
 # Measuring image quality
 
-The image quality metrics script uses multiple command line options. The following is an example where the script uses .JPEG reference images on images in the imagenet directory.
+The image quality metrics compares compressed image files with their decompressed output using [peak-signal-to-noise ratio (PSNR)](https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio) and [structural similarity index (SSIM)](https://en.wikipedia.org/wiki/Structural_similarity).
+
+## Examples
+
+The following is an example where the script uses .JPEG reference images on images in the imagenet directory.
 
 ```
 python3 img_quality.py -r .JPEG -i ../data/imagenet
--- metrics/img_quality.py
+++ metrics/img_quality.py
@@ -20,34 +20,34 @@ from statistics import mean
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def main():
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-
     parser = ArgumentParser(description="Collect image quality metrics.")
     parser.add_argument("--imgDir", "-i", help="directory in which to find images",
         default="../data")
-    parser.add_argument("--refFmt", "-r", help="filename extension/format of reference images",
-        default=".jpg")
+    parser.add_argument("--refFmt", "-r", help="regex for filename extension/format of reference images",
+        default="(?i)(.jpg|.jpeg|.png)")
     parser.add_argument("--outFmt", "-t", help="filename extension/format of output images",
         default=".bmp")
     parser.add_argument("--outputResults", "-o", help="filename extension/format of output images",
         default="img_quality_results.csv")
+    parser.add_argument("--PSNRonly", "-n", help="only calculate PSNR",
+        action="store_true")
     parser.add_argument("--PSNRthreshold", "-p", help="PSNR threshold to consider a file incorrect",
         type=float, default=30.0)
     parser.add_argument("--SSIMthreshold", "-s", help="SSIM threshold to consider a file incorrect",
         type=float, default=0.95)
     parser.add_argument("--verbosity", "-v", help="increase output verbosity",
         type=int, default=logging.INFO)
-    parser.add_argument("--quiet", "-q", help="do not print names of suspected error files",
-        action="store_true")
 
     args = parser.parse_args()
 
-    logging.disable(level=args.verbosity)
+    logging.basicConfig(stream=sys.stderr, level=args.verbosity)
 
     resultsFile = open(args.outputResults, 'w')
     results = csv.writer(resultsFile)
-    csvHeader = ['image', 'SSIM-R', 'SSIM-G', 'SSIM-B', 'SSIM',
-        'PSNR-R', 'PSNR-G', 'PSNR-B', 'PSNR',
+    csvHeader = ['image']
+    if not args.PSNRonly:
+        csvHeader += ['SSIM-R', 'SSIM-G', 'SSIM-B', 'SSIM']
+    csvHeader += ['PSNR-R', 'PSNR-G', 'PSNR-B', 'PSNR',
         'reference path', 'output path']
     results.writerow(csvHeader)
 
@@ -55,10 +55,20 @@ def main():
         for name in list(filter(lambda x: x[-len(args.outFmt):] == args.outFmt, files)):        
             outPath = os.path.join(path, name)
             # find reference image
-            refName = name[:-(len(args.outFmt)+4)] + args.refFmt
-            refPath = os.path.join(path, refName)
+            imgName = name[:-(len(args.outFmt)+4)]
 
-            if os.path.isfile(refPath):
+            ldir = os.listdir(path)
+            imgMatches = list(filter(lambda x:x.startswith(imgName), ldir))
+            refPath = None
+            pattern = re.compile(args.refFmt)
+
+            for f in imgMatches:
+                if pattern.search(f) != None:
+                    refName = f
+                    refPath = os.path.join(path, f)
+                    break
+
+            if refPath != None and os.path.isfile(refPath):
                 outImg = Image.open(outPath)
                 refImg = Image.open(refPath)
 
@@ -70,10 +80,10 @@ def main():
                     continue
 
                 if outImg.mode != refImg.mode:
-                    logging.warning("Image mode mismatch for image %s", name)
+                    logging.info("Image mode mismatch for image %s. Output converted to reference format.", name)
                     logging.debug("Output image mode: %s", outImg.mode)
                     logging.debug("Reference image mode: %s", refImg.mode)
-                    continue
+                    outImg = outImg.convert(refImg.mode)
 
                 try:
                     refPixels = np.asarray(refImg)
@@ -86,21 +96,48 @@ def main():
                     logging.warning("Output image %s corrupted with error: %s", name, e)
                     continue
 
-                ssim = [0.0]*4
-                psnr = [0.0]*4
+                if outPixels.shape != refPixels.shape:
+                    logging.warning("Image dimension mismatch for image %s", name)
+                    logging.debug("Output image dimensions: %s", str(outPixels.shape))
+                    logging.debug("Reference image dimensions: %s", str(refPixels.shape))
+                    logging.debug("Output image mode: %s", outImg.mode)
+                    logging.debug("Reference image mode: %s", refImg.mode)
+                    continue
 
-                for i in range(3):
-                    ssim[i] = metrics.structural_similarity(refPixels[:, :, i], outPixels[:, :, i])
-                    psnr[i] = metrics.peak_signal_noise_ratio(refPixels[:, :, i], outPixels[:, :, i])
+                if refImg.mode == "L":
+                    psnrl = metrics.peak_signal_noise_ratio(refPixels, outPixels)
+                    psnr = [psnrl]*4
+                    if not args.PSNRonly:
+                        ssiml = metrics.structural_similarity(refPixels, outPixels)
+                        ssim = [ssiml]*4
 
-                ssim[3] = mean(ssim[:3])
-                psnr[3] = mean(psnr[:3])
+                elif refImg.mode == "RGB":
+                    psnr = [0.0]*4
+                    for i in range(3):
+                        psnr[i] = metrics.peak_signal_noise_ratio(refPixels[:, :, i], outPixels[:, :, i])
+                    psnr[3] = mean(psnr[:3])
 
-                row = [refName] + ssim + psnr + [refPath, outPath]
+                    if not args.PSNRonly:
+                        ssim = [0.0]*4
+                        for i in range(3):
+                            ssim[i] = metrics.structural_similarity(refPixels[:, :, i], outPixels[:, :, i])
+                        ssim[3] = mean(ssim[:3])
+                else:
+                    logging.warning("Unsupported image mode for image %s", name)
+                    logging.debug("Image mode: %s", outImg.mode)
+                    continue
+
+                row = [refName]
+                if not args.PSNRonly:
+                    row += ssim
+                row += psnr + [refPath, outPath]
                 results.writerow(row)
 
-                if psnr[3] < args.PSNRthreshold or ssim[3] < args.SSIMthreshold:
-                    logging.warning("Output image %s may be corrupted.", name)
+                corrupted = psnr[3] < args.PSNRthreshold
+                if not args.PSNRonly:
+                    corrupted = corrupted or ssim[3] < args.SSIMthreshold
+                if corrupted:
+                    logging.info("Output image %s may be corrupted.", name)
             else:
                 logging.warning("Couldn't find reference image for: %s", outPath)
     
